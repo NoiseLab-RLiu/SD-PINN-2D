a324ae3c8206dbb103512cf13c95ed5a0d8cdf63
NoiseLab-RLiu <68029191+NoiseLab-RLiu@users.noreply.github.com>
2023-11-14 15:29:46 -0800

    Update modelLoss_2D_atten.m

--- modelLoss_2D_atten.m
+++ modelLoss_2D_atten.m
@@ -1,24 +1,16 @@
-function [loss,lossF,lossU,lossS,lossB,LAM1,LAM2,gradients, vU, PDEterms] = modelLoss_2D_atten(parameters,X,Y, T,X0, Y0, T0,U0,lenT,C,alpha, Weight,addon,numParams)
+function [loss,lossF,lossU,lossS,lossB,LAM1,LAM2,gradients] = modelLoss_2D_atten(parameters,XYT,X0Y0T0,U0,lenT,C,alpha, Weight,addon,numParams)
 % % Make predictions with the initial conditions.
-U = model_2D(parameters,X,Y,T,numParams);
-vU = var(U(:));
+U = model_2D(parameters,XYT,numParams);
 % Calculate derivatives with respect to X and T.
+X = XYT(1,:);
+Y = XYT(2,:);
+T = XYT(3,:);
 gradientsU = dlgradient(sum(U,"all"),{X,Y,T},EnableHigherDerivatives=true);
-Ux = gradientsU{1};
-Uy = gradientsU{2};
 Ut = gradientsU{3};
 %Calculate second-order derivatives with respect to X.
-Uxx = dlgradient(sum(Ux,"all"),X,EnableHigherDerivatives=true);
-Uyy = dlgradient(sum(Uy,"all"),Y,EnableHigherDerivatives=true);
+Uxx = dlgradient(sum(gradientsU{1},"all"),X,EnableHigherDerivatives=true);
+Uyy = dlgradient(sum(gradientsU{2},"all"),Y,EnableHigherDerivatives=true);
 Utt = dlgradient(sum(Ut,"all"),T,EnableHigherDerivatives=true);
-
-PDEterms = cell(6,1);
-PDEterms{1} = Ux;
-PDEterms{2} = Uy;
-PDEterms{3} = Ut;
-PDEterms{4} = Uxx;
-PDEterms{5} = Uyy;
-PDEterms{6} = Utt;
 
 % Calculate los
 LAM1 = parameters.Umat1*parameters.Vmat1;
@@ -31,10 +23,10 @@ lossF = mse(f, zeroTarget);
 lossS = sum(sum(relu(LAM1)))+sum(sum(relu(-LAM2))); % loss for the sign
 
 linearIndices = sub2ind(size(LAM1), addon(:, 1), addon(:, 2)); % Convert subscript indices to linear indices
-lossB = dlarray(sum((LAM1(linearIndices) + C(linearIndices).^2).^2) + sum((LAM2(linearIndices) - alpha(linearIndices)).^2));
+lossB = dlarray(sum( ( LAM1(linearIndices) + C(linearIndices).^2 ) .^2) + sum( ( LAM2(linearIndices) - alpha(linearIndices) ) .^2 ) );
 
 % Calculate lossU. 
-U0Pred = model_2D(parameters,X0,Y0,T0,numParams);
+U0Pred = model_2D(parameters,X0Y0T0,numParams);
 lossU = mse(U0Pred, U0);
 
 %Combine losses.
